d4a6d45711c16f9b2bd8efed6cabfdbe63aafe1d
Tuto <arthurgms098@gmail.com>
2025-09-12 17:04:15 -0300

    Primeiro commit cabeçalho do projeto

---
+++ T1.asm
@@ -0,0 +1,9 @@
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#	Trabalho 1 Organização e Arquitetura de Computadores SSC0902
+#	Alunos:					NUSP:
+#	Arthur Gomes Mesquita de Souza		14835157
+#	Nicolas Zimmer Fernandes		--------
+#	Luana Fialho				--------
+#	Lucas Moro Farias			--------
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
